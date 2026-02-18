54469a62829eb760dd3124175801555a28d104be
Jihi <jihi@jihi.com>
2026-02-18 18:31:15 +0100

    need to add monitoring and death and maybe something else who cares

--- src/routine.c
+++ src/routine.c
@@ -6,7 +6,7 @@
 /*   By: jihi <jihi@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/18 17:09:57 by jihi              #+#    #+#             */
-/*   Updated: 2026/02/18 17:19:57 by jihi             ###   ########.fr       */
+/*   Updated: 2026/02/18 18:29:32 by jihi             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,8 +30,13 @@ void	*philo_routine(void *ptr)
 	philo = (t_philo *)ptr;
 	while (!get_stop(philo->data))
 	{
-		print_state(philo->data, philo->id, "is thinking");
-		my_sleep(philo->data, 1000);
+		if (take_forks(philo))
+			break ;
+		eat(philo);
+		drop_forks(philo);
+		print_state(philo->data, philo->id, SLEEP);
+		my_sleep(philo->data, philo->data->t_to_sleep);
+		print_state(philo->data, philo->id, THINK);
 	}
 	return (NULL);
 }
